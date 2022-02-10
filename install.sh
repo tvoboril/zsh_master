f99bc0fcacdbbfd6febc030b0ee4bcc0522e3361
Tomas Voboril <tvoboril@users.noreply.github.com>
2022-02-10 09:19:44 -0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+mkdir ~/.font
+cp *.ttf ~/.font/.
+fc-cache -f -v
+
+#zsh setup
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+cp zshrc ~/.zshrc
+
+zsh p10k configure
