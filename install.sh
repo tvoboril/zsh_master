6464a589b59b7b12cde6a7166ffaed5b10b0a11d
Tomas Voboril <tvoboril@users.noreply.github.com>
2022-02-10 09:26:13 -0600

    silent omz

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ cp *.ttf ~/.font/.
 fc-cache -f -v
 
 #zsh setup
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
